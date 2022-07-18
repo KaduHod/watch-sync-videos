939725532bbcf03d32b3564ff501dfff25750190
Carlos <carlosjr.ribas@gmail.com>
2022-07-17 22:13:44 -0300

    caraio00000

--- assets/js/web-socket-client.js
+++ assets/js/web-socket-client.js
@@ -1,4 +1,4 @@
-const webSocket       = new WebSocket('ws://watch-sync-videos.herokuapp.com/')
+const webSocket       = new WebSocket('wss://watch-sync-videos.herokuapp.com/')
 const text            = document.getElementById('youtube-url')
 const botaoMudarVideo = document.getElementById('url-button')
 
