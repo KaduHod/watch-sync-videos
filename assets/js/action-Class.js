d639c7c0afec06b518678db0cfd67633c9de2f78
Carlos <carlosjr.ribas@gmail.com>
2022-07-19 21:47:16 -0300

    svg

---
+++ assets/js/action-Class.js
@@ -0,0 +1,17 @@
+class Action {
+    constructor(player){
+        this.player = player
+    }
+    switchVideo(){
+        changeVideo(dado)
+    }
+    pauseVideo(){
+        this.player.pauseVideo()
+    }
+    seekToo(){
+
+    }
+    runVideo(){
+        this.player.playVideo()
+    }
+}
